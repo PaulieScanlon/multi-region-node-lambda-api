268c56378c5a1d2d99332ada69c0dc95f09e5750
Paul Scanlon <pauliescanlon@gmail.com>
2023-05-26 11:06:41 +0100

    feat: add create function

---
+++ v1/create.js
@@ -0,0 +1,28 @@
+const { client } = require('../pg');
+
+module.exports.handler = async () => {
+  const date = new Date();
+  const region = `aws-${process.env.AWS_REGION}`;
+
+  try {
+    await client.connect();
+
+    await client.query('INSERT INTO data (date, region) VALUES($1, $2)', [date, region]);
+
+    await client.clean();
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: 'CREATE v1 - A Ok!',
+      }),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        message: 'CREATE v1 - Error!',
+      }),
+    };
+  }
+};
